56f7893a9f9b75a901d40322b4860ff026508b39
Jason Shin <visualbbasic@gmail.com>
2019-11-25 22:35:03 +1100

    initial rust commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+//! blursed-bot - blessed and blursed & cursed images
+#![deny(
+    missing_docs,
+    missing_debug_implementations,
+    missing_copy_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unused_import_braces,
+    unused_qualifications
+)]
+
+extern crate slack;
+
+fn main() {
+    println!("blursed!");
+}
