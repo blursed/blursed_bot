e3b719f61e97d15d2d7d4f10b2fa3fa4a6ae982d
Jason Shin <visualbbasic@gmail.com>
2020-11-16 09:31:16 +1100

    testing ref-cell

--- examples/rc.rs
+++ examples/rc.rs
@@ -1,5 +1,6 @@
 use std::rc::Rc;
 
+/*
 struct Person {
     name: Rc<String>,
 }
@@ -20,4 +21,24 @@ fn main() {
 
     person.greet();
     println!("hello again {}", name);
+}
+ */
+
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+
+fn main() {
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after create b {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
